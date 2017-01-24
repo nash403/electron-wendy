ddfae961c25b97f79c4be5af80ab47e32b4056be
nash403 <nash403@outlook.fr>
2017-01-24 23:41:33 +0100

    add test files

---
+++ test/test.js
@@ -0,0 +1,49 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+const Wendy = require('../index')
+
+// prevent window being garbage collected
+let win1, win2, win3, winTest
+
+function createMainWindow(name) {
+  const win = Wendy.create(name,{width: 600, height:400})
+  win.showUrl(path.join(__dirname,'test.html'), {index: name.split(' ')[1]}, ()=>{})
+  console.log(`new window created with index: ${win.id} and name: ${win.winName}`)
+
+  return win;
+}
+
+app.on('window-all-closed', () => {
+  console.log('closing app',Wendy.windows)
+  app.quit();
+});
+
+app.on('ready', () => {
+  win1 = createMainWindow('win 1');
+  win2 = createMainWindow('win 2');
+  win3 = createMainWindow('win 3');
+  console.log(`app has window 'win 2' ? => ${Wendy.has('win 2')}`)
+  console.log(`app has window 'inexistant' ? => ${Wendy.has('inexistant')}`)
+
+  console.log(`get window 'win 1' => ${Wendy.getByName('win 1').winName}`)
+  console.log(`get window 'inexistant' => ${Wendy.getByName('inexistant')}`)
+
+  console.log(`app has window 'test' ? => ${Wendy.has('test')}`)
+  console.log("addind window 'test'")
+  winTest = new BrowserWindow({
+    width: 600,
+    height: 400
+  });
+  winTest.loadURL(`file://${__dirname}/test.html`);
+  Wendy.add(winTest, 'test')
+  console.log(`app has window 'test' ? => ${Wendy.has('test')}`)
+
+  // Keep reference to handler to remove it later on. 
+  function handler (event) {
+    console.log(`${event.name} / ${event.data} / ${event.target} / ${event.emittedBy}`);
+  };
+  
+  // Registering listener. 
+  Wendy.on('my-event', handler);
+});
