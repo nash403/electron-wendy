5d77bfb041a87a6145017ea42dbecf75a4fcb82f
nash403 <nash403@outlook.fr>
2017-01-24 23:42:04 +0100

    feat() add eventEmitter for window communication

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const eventEmitter = new (require('events').EventEmitter)()
+const EventEmitter = new (require('events').EventEmitter)()
 const {dialog} = require('electron')
 const winmanager = require('electron-window')
 
 let  Wendy = {
   get windows() {
     return winmanager.windows;
+  },
+
+  emitter: EventEmitter,
+
+  on (evName, callback) {
+    this.emitter.on(evName, callback)
+    return this
+  },
+
+  off (evName, handler) {
+    this.emitter.removeListener(evName, handler)
+    return this
+  },
+
+  emit (evName, options) {
+    let data, target, sourceWin
+
+    if (options !== undefined && options !== null && typeof options === 'object') {
+      data = options.data;
+      target = options.target;
+      sourceWin = options.emittedBy;
+    }
+    this.emitter.emit(evName, {
+      emittedBy: this.getName(sourceWin),
+      target,
+      data,
+      name: evName
+    })
+    return this
   },
 
   create(name, options) {
@@ -17,6 +46,13 @@ let  Wendy = {
       win.winName = name
     }
     return win
+  },
+
+  getName (win) {
+    let res = winmanager.windows[Object.keys(winmanager.windows).find(key => {
+        return winmanager.windows[key] === win
+    })]
+    return res ? res.winName : null
   },
 
   getByName (name) {
